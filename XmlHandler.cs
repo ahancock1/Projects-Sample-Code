c7789348e640930d8fb9487a3b0420460eff8a1c
Adam Hancock <adam.hancock@moov.tv>
2015-06-05 15:38:48 +0100

    Added error handling and improved genericness

--- XmlHandler.cs
+++ XmlHandler.cs
@@ -1,65 +1,110 @@
-﻿using System;
+using System;
 using System.IO;
 using System.Xml.Serialization;
 
-namespace BT_Sport_Server
+namespace Hancock.Helpers
 {
     public static class XmlHandler
     {
-        public static object Deserialize(XmlSerializer serializer, string objectData)
-        {
-            object result;
-
-            using (TextReader reader = new StringReader(objectData))
+        public static T Deserialize<T>(XmlSerializer serializer, string objectData) where T : class
+        {         
+            try
             {
-                result = serializer.Deserialize(reader);
+                using (TextReader reader = new StringReader(objectData))
+                {
+                    return (T) serializer.Deserialize(reader);
+                }
+            }
+            catch (ArgumentNullException e)
+            {
+                Console.WriteLine(e.Message);
             }
 
-            return result;
+            return default(T);
         }
 
-        public static T DeserializeString<T>(string xml)
+        public static T DeserializeString<T>(string xml, XmlRootAttribute rootAttribute) where T : class
         {
-            return (T) DeserializeString(xml, typeof(T));
+            return Deserialize<T>(new XmlSerializer(typeof(T), rootAttribute), xml);
         }
 
-        public static T DeserializeString<T>(string xml, XmlRootAttribute rootAttribute)
+        public static T DeserializeString<T>(string xml) where T : class
         {
-            return (T) DeserializeString(xml, typeof(T), rootAttribute);
-        }
+            return Deserialize<T>(new XmlSerializer(typeof(T)), xml);
+        }              
 
-        public static object DeserializeString(string xml, Type type)
+        public static T DeserializeFile<T>(string filePath) where T : class
         {
-            return Deserialize(new XmlSerializer(type), xml);
-        }
-
-        public static object DeserializeString(string xml, Type type, XmlRootAttribute rootAttribute)
-        {
-            return Deserialize(new XmlSerializer(type, rootAttribute), xml);
-        }
-
-        public static T DeserializeFile<T>(string filePath)
-        {
-            string text = String.Empty;
-
-            using (StreamReader reader = new StreamReader(filePath))
+            try
             {
-                text = reader.ReadToEnd();
+                using (StreamReader reader = new StreamReader(filePath))
+                {
+                    DeserializeString<T>(reader.ReadToEnd());
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error deserializing file {0}, {1}", filePath, e.Message);
             }
 
-            return (T) DeserializeString(text, typeof(T));
+            return default(T);
         }
 
-        public static T DeserializeFile<T>(string filePath, XmlRootAttribute rootAttribute)
+        public static T DeserializeFile<T>(string filePath, XmlRootAttribute rootAttribute) where T : class
         {
-            string text = String.Empty;
-
-            using (StreamReader reader = new StreamReader(filePath))
+            try
             {
-                text = reader.ReadToEnd();
+                using (StreamReader reader = new StreamReader(filePath))
+                {
+                    DeserializeString<T>(reader.ReadToEnd(), rootAttribute);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error deserializing file {0}, {1}", filePath, e.Message);
             }
 
-            return (T) DeserializeString(text, typeof(T), rootAttribute);
+            return default(T);
+        }
+
+        public static void WriteFile<T>(this T data, string filePath)
+        {
+            try
+            {
+                XmlSerializer serializer = new XmlSerializer(typeof(T));
+
+                if (!Directory.Exists(Path.GetDirectoryName(filePath)))
+                {
+                    Directory.CreateDirectory(Path.GetDirectoryName(filePath));
+                }
+                using (StreamWriter writer = new StreamWriter(filePath))
+                {
+                    serializer.Serialize(writer, data);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error creating {0} xml file at {1}, {2}", data, filePath, e.Message);
+            }
+        }
+
+        public static string SerializeString<T>(this T data)
+        {
+            try
+            {
+                XmlSerializer serializer = new XmlSerializer(typeof(T));
+
+                using (StringWriter writer = new StringWriter())
+                {
+                    serializer.Serialize(writer, data);
+                    return writer.ToString();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error serializing {0} to string, {1}", data, e.Message);
+            }
+            return String.Empty;
         }
     }
 }
